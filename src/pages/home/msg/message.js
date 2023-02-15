7c9b0df6dcd625a66c4533ed30cf39cfce43df32
Rune-Coder <souhardya.dutta7801@gmail.com>
2023-02-15 10:36:56 +0530

    mark 8

--- src/pages/home/msg/message.js
+++ src/pages/home/msg/message.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import classes from './chats.module.css';
+import { useSelector } from 'react-redux';
+import classes from './message.module.css';
 
 function Message(props){
+    const userSub = useSelector((state) => state.login.loginData);
+
+    var time = new Date(props.msgData.updatedAt);
+    var hours = time.getHours();
+    hours = (hours > 12) ? hours-12: hours;
+    var min = time.getMinutes();
+    var meridiem = (hours >= 12) ? "PM": "AM";
+
+
     return(
-        <div>
-            {props.msgData.message}
+        <div className={classes.msgContainer}>
+            <div className = {`${userSub.sub === props.msgData.receiverId && classes.receiverMsg}  ${userSub.sub !== props.msgData.receiverId && classes.senderMsg}`} >
+                {props.msgData.message}
+                <div className={classes.msgTime}>{hours+":"+min+" "+meridiem}</div>
+            </div>
         </div>
     );
 }
