d3c785ab59c06ddf438d89fd65444b3f50431aea
Rune-Coder <souhardya.dutta7801@gmail.com>
2023-02-17 20:48:38 +0530

    mark 13

--- src/pages/home/msg/chatHeader.js
+++ src/pages/home/msg/chatHeader.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import classes from './chatHeader.module.css';
+import { senderActions } from '../../../store/senderStore';
 
 function ChatHeader(props){
 
     const senderSub = useSelector((state) => state.senders.senderData);
     const activeUsers = useSelector((state) => state.login.activeUsers);
+    
+    const dispatch = useDispatch();
+
+    function backHandler(event){
+        dispatch(senderActions.person({senderData: {}}));
+    }
 
     return(
         <div className={classes.chatsHeaderContainer}>
             <ul className={classes.profileList}>
+                <li className={classes.backArrow} onClick={backHandler}>
+                    <ArrowBackIcon />
+                </li>
                 <li>
                     <img className={classes.profilePic} src={senderSub.dp} alt="Profile"></img>
                 </li>
